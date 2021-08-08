099c6ad1e3e0932c0e95fc67af07dd4ae9dfaf45
Stepan <sssstepa452@yandex.ru>
2021-08-08 14:31:52 +0300

    Moved files from autorun to nls/ folder

--- lua/nls/_include.lua
+++ lua/nls/_include.lua
@@ -12,3 +12,17 @@ include("servermenu.lua")
 include("gamemodes.lua")
 
 include("zadalbot.lua")
+
+if CLIENT then
+    include("lzwd_cl.lua")
+else
+    AddCSLuaFile("lzwd_cl.lua")
+    include("lzwd_cfg_sv.lua")
+    include("lzwd_sv.lua")        
+end
+
+if SERVER then
+    AddCSLuaFile("entity_infobox_cl.lua")
+else
+    include("entity_infobox_cl.lua")
+end
