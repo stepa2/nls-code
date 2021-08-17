a86c3b6b762831fcef449bc8831093dadee32ca9
Stepan <sssstepa452@yandex.ru>
2021-08-17 16:40:35 +0300

    Fixed gamemode commands access, added help

--- lua/ulx/modules/sh/nls_commands.lua
+++ lua/ulx/modules/sh/nls_commands.lua
@@ -1,9 +1,15 @@
 AddCSLuaFile()
 
-ulx.command("NLS", "ulx pvp", function(ply)
+local cmd_pvp = ulx.command("NLS", "ulx pvp", function(ply)
     NLS.Gamemodes.Set(ply, NLS.Gamemodes.Types.PVP)
 end, {"!pvp"})
 
-ulx.command("NLS", "ulx build", function(ply)
+cmd_pvp:defaultAccess(ULib.ACCESS_ALL)
+cmd_pvp:help("Переводит игрока в режим боя")
+
+local cmd_build = ulx.command("NLS", "ulx build", function(ply)
     NLS.Gamemodes.Set(ply, NLS.Gamemodes.Types.BUILD)
 end, {"!build"})
+
+cmd_build:defaultAccess(ULib.ACCESS_ALL)
+cmd_build:help("Переводит игрока в режим строительства")
