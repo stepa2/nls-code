e91b4d0bcfb66505f3396dd0877cae2c94a8693d
Stepan <sssstepa452@yandex.ru>
2021-10-22 23:53:50 +0300

    Added ability to set other players' gamemode for admins

--- lua/ulx/modules/sh/nls_commands.lua
+++ lua/ulx/modules/sh/nls_commands.lua
@@ -1,15 +1,35 @@
 AddCSLuaFile()
 
-local cmd_pvp = ulx.command("NLS", "ulx pvp", function(ply)
-    NLS.Gamemodes.Set(ply, NLS.Gamemodes.Types.PVP)
+local cmd_pvp = ulx.command("NLS", "ulx pvp", function(caller, target)
+    if IsValid(target) then
+        if not caller:IsAdmin() then
+            ulx.fancyLogAdmin( caller, false, "Только администрация может менять игровые режимы других игроков" )
+            return
+        end
+    else
+        target = caller
+    end
+
+    NLS.Gamemodes.Set(target, NLS.Gamemodes.Types.PVP)
 end, {"!pvp"})
 
+cmd_pvp:addParam{ type=ULib.cmds.PlayerArg, ULib.cmds.optional }
 cmd_pvp:defaultAccess(ULib.ACCESS_ALL)
 cmd_pvp:help("Переводит игрока в режим боя")
 
-local cmd_build = ulx.command("NLS", "ulx build", function(ply)
-    NLS.Gamemodes.Set(ply, NLS.Gamemodes.Types.BUILD)
+local cmd_build = ulx.command("NLS", "ulx build", function(caller, target)
+    if IsValid(target) then
+        if not caller:IsAdmin() then
+            ulx.fancyLogAdmin( caller, false, "Только администрация может менять игровые режимы других игроков" )
+            return
+        end
+    else
+        target = caller
+    end
+
+    NLS.Gamemodes.Set(target, NLS.Gamemodes.Types.BUILD)
 end, {"!build"})
 
+cmd_pvp:addParam{ type=ULib.cmds.PlayerArg, ULib.cmds.optional }
 cmd_build:defaultAccess(ULib.ACCESS_ALL)
 cmd_build:help("Переводит игрока в режим строительства")
