755ae386cb0c2d5fa6ed23fe6c6fa69d9afd4625
Stepan <sssstepa452@yandex.ru>
2021-10-24 00:29:54 +0300

    Gamemode system improvements

--- lua/nls/gamemodes.lua
+++ lua/nls/gamemodes.lua
@@ -1,17 +1,26 @@
 AddCSLuaFile()
 
+if SERVER then
+    util.AddNetworkString("NLS_Gamemode_Changed")
+end
+
 local Gamemodes = {}
 NLS.Gamemodes = Gamemodes
 
 Gamemodes.Types = {
     PVP = 1,
     BUILD = 2,
-    --RP = 3,
+    RP = 3,
     [1] = "PVP",
     [2] = "BUILD",
-    --[3] = "RP"
+    [3] = "RP"
 }
 
+Gamemodes.FancyName = {
+    [1] = "PvP",
+    [2] = "Build",
+    [3] = "RP"
+}
 
 if SERVER then
     local PlayerGamemodes = {}
@@ -20,17 +29,24 @@ if SERVER then
         if gm == PlayerGamemodes[ply] then
             return
         end
-        
+
         if PlayerGamemodes[ply] ~= nil and gm ~= Gamemodes.Types.BUILD then
             ply:Spawn()
         end
 
+        ply:SetNoTarget(gm == Gamemodes.Types.Build)
+
         ply:SetNWInt("NLS_Gamemode", gm)
         ply:EmitSound("buttons/button24.wav")
+
+        net.Start("NLS_Gamemode_Changed")
+            net.WriteEntity(ply)
+        net.Broadcast()
+
         PlayerGamemodes[ply] = gm
     end
 
-    function NLS.Gamemodes.Get(ply)
+    function Gamemodes.Get(ply)
         return PlayerGamemodes[ply]
     end
 
@@ -46,77 +62,105 @@ if SERVER then
         Gamemodes.Set(ply, nil)
     end)
 else
-    function NLS.Gamemodes.Get(ply)
+    function Gamemodes.Get(ply)
         return ply:GetNWInt("NLS_Gamemode")
     end
+
+    net.Receive("NLS_Gamemode_Changed", function()
+        local ply = net.ReadEntity()
+
+        chat.AddText(SERVER_NAME_COLOR, "NLS: ",
+            Color(255,255,255), "Игрок ", ply, " сменил игровой режим на ", Gamemodes.GetFancyName(ply))
+    end)
+end
+
+
+function Gamemodes.GetFancyName(ply)
+    return Gamemodes.FancyName[Gamemodes.Get(ply)] or "Invalid"
 end
 
 if SERVER then
+    local function GetDamageRelatedPlayer(ent)
+        if not IsValid(ent) then
+            return nil
+        end
+
+        if ent:IsPlayer() then
+            return ent
+        end
+
+        local owner = ent:GetOwner()
+        if IsValid(owner) then
+            return GetDamageRelatedPlayer(owner)
+        end
+
+        local creator = ent:GetCreator()
+        if IsValid(creator) then
+            return GetDamageRelatedPlayer(creator)
+        end
+
+        return nil
+    end
 
     local function TakeDamageBy(target, damager)
         --if not IsValid(target) or not IsValid(damager) then
         --    return false
         --end
-        
-        local targetPly = target
-    
-        if not targetPly:IsPlayer() then
-            targetPly = targetPly:GetOwner()
+
+        print("Damage", "Target", target, "Damager", damager)
+
+        local targetPly = GetDamageRelatedPlayer(target, function() end)
+        local damagerPly = GetDamageRelatedPlayer(damager, print)
+
+        print("TargetPly", targetPly, "DamagerPly", damagerPly)
+
+        if damagerPly == nil then
+            return
+                (targetPly == nil) or
+                (NLS.Gamemodes.Get(targetPly) ~= Gamemodes.Types.BUILD)
         end
-    
-        if targetPly ~= nil and not targetPly:IsPlayer() then
-            targetPly = nil
-        end 
-    
-        local damagerPly = damager
-    
-        if IsValid(damagerPly) and not damagerPly:IsPlayer() then
-            damagerPly = damagerPly:GetOwner()
+
+        local damagerMode = NLS.Gamemodes.Get(damagerPly)
+
+        if targetPly == nil then
+            return damagerMode ~= Gamemodes.Types.BUILD
         end
-    
-        if damagerPly ~= nil and not damagerPly:IsPlayer() then
-            damagerPly = nil
-        end 
-    
-        if damagerPly ~= nil then
-            local damagerMode = NLS.Gamemodes.Get(damagerPly)
-          
-            if targetPly == nil then
-                return damagerMode ~= Gamemodes.Types.BUILD
-            end
-    
-            local targetMode = NLS.Gamemodes.Get(targetPly)
-    
-    
-            if targetMode == Gamemodes.Types.BUILD then
-                return damagerPly == targetPly
-            elseif targetMode == Gamemodes.Types.PVP then
-                return damagerMode == Gamemodes.Types.PVP
-            else -- Gamemodes.Types.RP
-               if damagerMode ~= Gamemodes.Types.RP then
-                   return false
-               end
-    
-               -- TODO: roleplay groups
-               return true
-            end
-        else
-            if targetPly == nil then
-                return true
-            end
-    
-            return NLS.Gamemodes.Get(targetPly) ~= Gamemodes.Types.BUILD
+
+        local targetMode = NLS.Gamemodes.Get(targetPly)
+
+
+        if targetMode == Gamemodes.Types.BUILD then
+            return
+                (not target:IsPlayer()) and
+                damagerPly == targetPly
         end
+
+        if targetMode ~= damagerMode then
+            return false
+        end
+
+
+        if targetMode == Gamemodes.Types.PVP then
+            return true
+        end
+
+        -- TODO: roleplay groups
+       return true
     end
-    
+
+    hook.Add("PlayerShouldTakeDamage", "NLS_Gamemodes", function(target, damager)
+        if not TakeDamageBy(target, damager) then
+            return false
+        end
+    end)
+
     hook.Add("EntityTakeDamage", "NLS_Gamemodes", function(target, dmg)
         local damager = dmg:GetInflictor()
-    
+
         if not TakeDamageBy(target, damager) then
             return true
         end
     end)
-
 end
 
 
@@ -131,11 +175,11 @@ local function AllowNoclip(ply)
         return true
     end
 
-    if plyMode == Gamemodes.Types.RP then
-        -- TODO: roleplay custom rules (GMing)
-
+    if plyMode == Gamemodes.Types.PVP then
         return false
     end
+
+    -- TODO: roleplay custom rules (GMing)
 
     return false
 end
