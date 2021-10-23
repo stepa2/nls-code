80af60cfbca6860f616304fefbbcac26fb972f70
Stepan <sssstepa452@yandex.ru>
2021-10-23 23:28:37 +0300

    Improved 3D nicknames

--- lua/nls/player_3dnick_cl.lua
+++ lua/nls/player_3dnick_cl.lua
@@ -1,20 +1,51 @@
-surface.CreateFont( "bigHud", {
+surface.CreateFont( "NLS_3DNick", {
 	font = "Open Sans Bold",
 	size = 70,
 	weight = 500,
 	entialias = true
 } )
 
-local function nameTop ( ply )
+local function Get3DNickPos(ply)
+	local attach = ply:LookupAttachment("anim_attachment_head")
 
-	if !IsValid ( ply ) then return end
-	
-	local color = team.GetColor ( ply:Team() )
-	
-	cam.Start3D2D( ply:GetPos() + Vector( 0, 0, 85), Angle( 0, RenderAngles().y - 90, 90 ), 0.1 )
-		draw.SimpleTextOutlined ( ply:Name(), "bigHud", 0, 0, color, TEXT_ALIGN_CENTER, 0, 2, Color( 255, 255, 255, 255 ) )
-	cam.End3D2D()
+	if attach <= 0 then
+		return ply:GetPos() + Vector(0,0,90)
+	end
 
+	return ply:GetAttachment(attach).Pos + Vector(0,0,30)
 end
 
-hook.Add( "PostPlayerDraw", "nameTop", nameTop )
+local COLOR_TEXT_DEFAULT = Color(255,255,255)
+local COLOR_OUTLINE = Color(0,0,0)
+
+hook.Add( "PostPlayerDraw", "NLS_3DNick", function(ply, flags)
+	if not IsValid(ply) then return end
+	if not ply:Alive() then return end
+
+	local plyTeam = ply:Team()
+	local plyTeamName = team.GetName(plyTeam)
+	if plyTeamName == "" then plyTeamName = "ЕГГОГ" end
+	local plyTeamColor = team.GetColor(plyTeam) or Color(255,0,0)
+
+
+
+	local pos = Get3DNickPos(ply)
+	cam.Start3D2D( pos, Angle( 0, RenderAngles().y - 90, 90 ), 0.1 )
+		draw.SimpleTextOutlined ( ply:Name(),
+			"NLS_3DNick",  0, 0, COLOR_TEXT_DEFAULT,
+			TEXT_ALIGN_CENTER, TEXT_ALIGN_LEFT,
+			1, COLOR_OUTLINE )
+
+		draw.SimpleTextOutlined ( plyTeamName,
+			"NLS_3DNick",  0, 60, plyTeamColor,
+			TEXT_ALIGN_CENTER, TEXT_ALIGN_LEFT,
+			1, COLOR_OUTLINE )
+
+		draw.SimpleTextOutlined ( NLS.Gamemodes.GetFancyName(ply),
+			"NLS_3DNick",  0, 120, COLOR_TEXT_DEFAULT,
+			TEXT_ALIGN_CENTER, TEXT_ALIGN_LEFT,
+			1, COLOR_OUTLINE )
+	cam.End3D2D()
+end)
+
+hook.Add("HUDDrawTargetID", "NLS_3DNick", function() return false end)
