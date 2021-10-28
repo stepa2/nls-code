3c94f3c4c6b5325007360d4fecf3e70e7f618eed
Stepan <sssstepa452@yandex.ru>
2021-10-28 16:00:32 +0300

    Fixed ENTITY:GetCreator() fix

--- lua/nls/bugfixes.lua
+++ lua/nls/bugfixes.lua
@@ -1,21 +1,27 @@
-local function PlayerSpawnedEngineEnt(ply, ent)
-    if ent:GetCreator() ~= ply then
-        ent:SetCreator(ply)
+AddCSLuaFile()
+
+if SERVER then
+
+    local function PlayerSpawnedEngineEnt(ply, ent)
+        if ent:GetCreator() ~= ply then
+            ent:SetCreator(ply)
+        end
     end
-end
 
-local function PlayerSpawnedModel(ply, model, ent)
-    PlayerSpawnedEngineEnt(ply, ent)
-end
+    local function PlayerSpawnedModel(ply, model, ent)
+        PlayerSpawnedEngineEnt(ply, ent)
+    end
 
-local ENGINE_ENT_HOOKS = {
-    PlayerSpawnedEffect = PlayerSpawnedModel,
-    PlayerSpawnedNPC = PlayerSpawnedEngineEnt,
-    PlayerSpawnedProp = PlayerSpawnedModel,
-    PlayerSpawnedRagdoll = PlayerSpawnedModel,
-    PlayerSpawnedVehicle = PlayerSpawnedEngineEnt
-}
+    local ENGINE_ENT_HOOKS = {
+        PlayerSpawnedEffect = PlayerSpawnedModel,
+        PlayerSpawnedNPC = PlayerSpawnedEngineEnt,
+        PlayerSpawnedProp = PlayerSpawnedModel,
+        PlayerSpawnedRagdoll = PlayerSpawnedModel,
+        PlayerSpawnedVehicle = PlayerSpawnedEngineEnt
+    }
 
-for hookname, hookfn in pairs(ENGINE_ENT_HOOKS) do
-    hook.Add(hookname, "NLS_Bugfix_Creator", hookfn)
+    for hookname, hookfn in pairs(ENGINE_ENT_HOOKS) do
+        hook.Add(hookname, "NLS_Bugfix_Creator", hookfn)
+    end
+
 end
