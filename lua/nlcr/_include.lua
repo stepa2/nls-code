bcad1f83e0b605c8d2b3dfe816fa9cdb60789f34
Stepan <sssstepa452@yandex.ru>
2021-12-02 00:09:46 +0300

    Exported NLCR.IncludeAll, added NLCR.IncludeFile

--- lua/nlcr/_include.lua
+++ lua/nlcr/_include.lua
@@ -11,26 +11,30 @@ end
 
 NLCR = {}
 
-local function IncludeAll(files)
-    for i, filename in ipairs(files) do
-        local ends_with_sv = string.EndsWith(filename, "_sv.lua")
-        local ends_with_cl = string.EndsWith(filename, "_cl.lua")
-        -- _sh.lua handled as *anything else*.lua
+function NLCR.IncludeFile(filename)
+    local ends_with_sv = string.EndsWith(filename, "_sv.lua")
+    local ends_with_cl = string.EndsWith(filename, "_cl.lua")
+    -- _sh.lua handled as *anything else*.lua
 
-        if not ends_with_sv and SERVER then
-            AddCSLuaFile(filename)
-        end
+    if not ends_with_sv and SERVER then
+        AddCSLuaFile(filename)
+    end
 
-        if  (ends_with_sv and SERVER) or
-            (ends_with_cl and CLIENT) or
-            (not ends_with_sv and not ends_with_cl)
-        then
-            include(filename)
-        end
+    if  (ends_with_sv and SERVER) or
+        (ends_with_cl and CLIENT) or
+        (not ends_with_sv and not ends_with_cl)
+    then
+        return include(filename)
     end
 end
 
-IncludeAll({
+function NLCR.IncludeAll(files)
+    for i, filename in ipairs(files) do
+        NLCR.IncludeFile(filename)
+    end
+end
+
+NLCR.IncludeAll({
     "bugfixes.lua",
     "spawnmenu_cl.lua",
     "lzwd_cl.lua",
