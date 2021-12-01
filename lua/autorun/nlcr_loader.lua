aaa5b0b02fa69db97fbdfc4da54690b9ec47735c
Stepan <sssstepa452@yandex.ru>
2021-12-02 00:40:48 +0300

    Added NLCR.AfterInit hook

--- lua/autorun/nlcr_loader.lua
+++ lua/autorun/nlcr_loader.lua
@@ -5,3 +5,5 @@ print("NLCR > Initialization started "..(CLIENT and "clientside" or "serverside"
 include("nlcr/_include.lua")
 
 print("NLCR > Initialization finished")
+
+hook.Run("NLCR.AfterInit")
