3ebcfc31b5cb07694e88a90e821f45dc1772a085
Stepan <sssstepa452@yandex.ru>
2021-12-25 18:20:41 +0300

    Added database config

---
+++ lua/nlcr/configs/core_database.lua
@@ -0,0 +1,10 @@
+return {
+    Adapter = "sqlite",
+    --[[
+        Host = "localhost",
+        Port = 6666,
+        User = "username",
+        Password = "qwertyuiop", -- use file.Read to not include password into VCS
+        DatabaseName = "ExampleServerDB"
+    ]]
+}
